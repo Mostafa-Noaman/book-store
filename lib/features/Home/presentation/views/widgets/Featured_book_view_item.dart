9e8e006e453535cdda6eec4f2d4ac373b2ccd342
Mostafa Noaman <nondy1425@gmail.com>
2024-01-06 19:43:50 +0200

    fixed the item height

--- lib/features/Home/presentation/views/widgets/Featured_book_view_item.dart
+++ lib/features/Home/presentation/views/widgets/Featured_book_view_item.dart
@@ -7,17 +7,14 @@ class FeaturedBooksViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.6 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: const DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(AssetsData.testImage),
-            ),
+    return AspectRatio(
+      aspectRatio: 2.6 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.testImage),
           ),
         ),
       ),
